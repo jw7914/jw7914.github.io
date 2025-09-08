a8a615e42eca57c19368656496485f42bc14b192
jw7914 <jw7914@nyu.edu>
2025-09-08 05:29:38 -0400

    inital skills section with icons

--- src/App.jsx
+++ src/App.jsx
@@ -164,15 +164,43 @@ const App = () => {
           items={[
             {
               quote: "JavaScript",
+              icon: (
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+                  alt="JavaScript"
+                  className="w-8 h-8 inline-block mr-2 align-middle"
+                />
+              ),
             },
             {
               quote: "HTML",
+              icon: (
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+                  alt="HTML5"
+                  className="w-8 h-8 inline-block mr-2 align-middle"
+                />
+              ),
             },
             {
               quote: "CSS",
+              icon: (
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+                  alt="CSS3"
+                  className="w-8 h-8 inline-block mr-2 align-middle"
+                />
+              ),
             },
             {
               quote: "Python",
+              icon: (
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+                  alt="Python"
+                  className="w-8 h-8 inline-block mr-2 align-middle"
+                />
+              ),
             },
           ]}
         />
