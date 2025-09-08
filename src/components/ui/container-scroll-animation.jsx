6fd7c49cb6160f5062b51f73d9471a87b97e6e7c
jw7914 <jw7914@nyu.edu>
2025-09-08 04:45:42 -0400

    finished scroller container

--- src/components/ui/container-scroll-animation.jsx
+++ src/components/ui/container-scroll-animation.jsx
@@ -70,7 +70,7 @@ export const Card = ({ rotate, scale, children }) => {
         boxShadow:
           "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
       }}
-      className="w-full sm:w-[98vw] md:w-[96vw] max-w-md sm:max-w-4xl md:max-w-6xl lg:max-w-[90vw] -mt-8 sm:-mt-16 mx-auto h-[48rem] md:h-[80rem] border-4 border-[#6C6C6C] p-2 sm:p-4 md:p-12 bg-[#222222] rounded-[24px] sm:rounded-[32px] md:rounded-[40px] shadow-2xl"
+      className="w-[98vw] sm:w-[96vw] md:w-[90vw] max-w-md sm:max-w-4xl md:max-w-5xl lg:max-w-[70vw] -mt-8 sm:-mt-16 mx-auto h-[52rem] sm:h-[56rem] md:h-[64rem] border-4 border-[#6C6C6C] p-2 sm:p-4 md:p-12 bg-[#222222] rounded-[24px] sm:rounded-[32px] md:rounded-[40px] shadow-2xl"
     >
       <div className="h-full w-full overflow-hidden rounded-xl sm:rounded-2xl md:rounded-2xl p-2 sm:p-4 md:p-8 bg-gray-100 dark:bg-zinc-900">
         {children}
