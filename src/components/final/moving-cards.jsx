0c3b202170bdb3399a0f0f1c51d0cb6867d4bce1
jw7914 <jw7914@nyu.edu>
2025-09-08 16:05:46 -0400

    adding styling to skills cards

--- src/components/final/moving-cards.jsx
+++ src/components/final/moving-cards.jsx
@@ -6,7 +6,7 @@ import { InfiniteMovingCards } from "../ui/infinite-moving-cards";
 export function MovingSkillCards() {
   return (
     <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
-      <InfiniteMovingCards items={skills} direction="right" speed="slow" />
+      <InfiniteMovingCards items={skills} direction="right" speed="medium" />
     </div>
   );
 }
