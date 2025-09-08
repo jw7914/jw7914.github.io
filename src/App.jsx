f24847df2f62b9d71ee15426cb0127ed1cb306cc
jw7914 <jw7914@nyu.edu>
2025-09-08 17:00:21 -0400

    renaming variables

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ import { MovingSkillCards } from "./components/final/moving-cards";
 
 const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
 const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC;
 
 const App = () => {
   const form = useRef();
